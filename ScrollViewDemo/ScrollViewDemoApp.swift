a6037b7b3a06106711bb1fa63cf92137c6e9210b
Kyle Musco <kmusco@ycp.edu>
2021-05-24 19:38:09 -0400

    Initial Commit

---
+++ ScrollViewDemo/ScrollViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewDemoApp.swift
+//  ScrollViewDemo
+//
+//  Created by Kyle Musco on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
